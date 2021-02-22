09fff8cb6cffa5723746f942c401a2cb1e12ec1c
Pratkkumar24 <pratikkumar200024@gmail.com>
2021-02-22 23:31:19 +0530

    changes in modules

--- Jarvis/Modules/module.py
+++ Jarvis/Modules/module.py
@@ -5,4 +5,10 @@ import speech_recognition as sr
 import wikipedia
 import sys
 import re
-import socket
+import speedtest
+import webbrowser
+import socket, requests
+from youtubesearchpython import VideosSearch
+from win10toast import ToastNotifier
+import time
+from PyDictionary import PyDictionary
