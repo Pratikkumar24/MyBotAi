9b74d8c8ec0336a88c0c7ee9808dd19471e9d709
Pratkkumar24 <pratikkumar200024@gmail.com>
2021-02-23 20:18:45 +0530

    this is just a trail python program

--- Jarvis/check.py
+++ Jarvis/check.py
@@ -1,12 +1,25 @@
 from PyDictionary import PyDictionary
+import re
+dic = PyDictionary()
 
-dictionary=PyDictionary("night","ambush","nonchalant","perceptive")
+# result = dic.meaning("telephone")
+# print(result['Noun'])
 
-print("Printing the meaning:")
-print(dictionary.printMeanings()) 
-print("\nPrinting the meaning according to dictionary")
-print(dictionary.getMeanings()) 
-print("\nPrinting the Synonyms:")
-print (dictionary.getSynonyms())
-print("\nTranslating them in hindi:")
-print (dictionary.translateTo("hi"))
+query = " what does phsdfsf means "
+useless = re.findall("(?:)(.*what)", query)[0]
+query = query.replace(useless,"")
+query = re.findall(r"(\w{3,})( means?)", query)[0][0]
+
+
+print(query)
+# dictionary=PyDictionary("hotel","ambush","nonchalant","perceptive")
+
+# print()
+# print("Printing the meaning:")
+# print(dictionary.printMeanings()) 
+# print("\nPrinting the meaning according to dictionary")
+# print(dictionary.getMeanings()) 
+# print("\nPrinting the Synonyms:")
+# print (dictionary.getSynonyms())
+# print("\nTranslating them in hindi:")
+# print (dictionary.translateTo("hi"))
